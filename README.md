b20f4ccf7d79154f98fdb8607aeccd09fcb79c18
Bedirhan Saglam <bedirhansaglam270@gmail.com>
2024-02-23 23:47:24 +0300

    update readme

--- README.md
+++ README.md
@@ -83,6 +83,30 @@ base mixin ViewModelMixin {
 }
 ```
 
+#### HomeService
+
+```dart
+/// Defining a final class HomeService
+final class HomeService {
+  /// Constructor for HomeService which takes an instance of FirebaseService
+  HomeService(FirebaseService firebaseService) : _firebaseService = firebaseService;
+
+  /// Declaring a private variable _firebaseService of type FirebaseService
+  final FirebaseService _firebaseService;
+
+  /// Defining a method fetchHomeList which returns a Future that may resolve to a tuple containing a List of HomeModel and a Failure object
+  Future<(List<HomeModel>?, Failure?)> fetchHomeList() async {
+    // Calling the fetchDocs method of _firebaseService
+    return _firebaseService.fetchDocs<HomeModel, List<HomeModel>>(
+      // Providing the fromJson function to convert fetched data to HomeModel instances
+      fromJson: HomeModel.fromJson,
+      // Providing the path to the collection from which data needs to be fetched
+      path: CollectionEnums.list,
+    );
+  }
+}
+```
+
 #### HomeViewModel
 
 ```dart
--- lib/feature/home/service/home_service.dart
+++ lib/feature/home/service/home_service.dart
@@ -3,13 +3,21 @@ import 'package:flutter_record_and_firebase_service/product/base/firebase/fireba
 import 'package:flutter_record_and_firebase_service/product/base/model/failure.dart';
 import 'package:flutter_record_and_firebase_service/product/enums/product_enums.dart';
 
+/// Defining a final class HomeService
 final class HomeService {
+  /// Constructor for HomeService which takes an instance of FirebaseService
   HomeService(FirebaseService firebaseService) : _firebaseService = firebaseService;
+
+  /// Declaring a private variable _firebaseService of type FirebaseService
   final FirebaseService _firebaseService;
 
+  /// Defining a method fetchHomeList which returns a Future that may resolve to a tuple containing a List of HomeModel and a Failure object
   Future<(List<HomeModel>?, Failure?)> fetchHomeList() async {
+    // Calling the fetchDocs method of _firebaseService
     return _firebaseService.fetchDocs<HomeModel, List<HomeModel>>(
+      // Providing the fromJson function to convert fetched data to HomeModel instances
       fromJson: HomeModel.fromJson,
+      // Providing the path to the collection from which data needs to be fetched
       path: CollectionEnums.list,
     );
   }
